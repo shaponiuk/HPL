f5f7b77eb52ee0eb4899342f53ca98cb2df3a1c4
SzymonHaponiuk <shaponiuk@linux-1.home>
2020-04-23 17:51:58 +0200

    .

--- src/StaticCheck/Format.hs
+++ src/StaticCheck/Format.hs
@@ -1,3 +1,19 @@
 module StaticCheck.Format where
 
-data ProgramFormat = TODO deriving (Show)
+newtype IdentF = IdentF String
+  deriving (Show)
+
+data ProgramFormat = SITList [SIT]
+  deriving (Show)
+
+data SIT = SITStruct FStruct | SITInterface FInterface | SITType FAlgType
+  deriving (Show)
+
+data FStruct = FStructB Ident [FInterfaceId] FStructBody
+  deriving (Show)
+
+data FInterface = FInterface
+  deriving (Show)
+ 
+data FAlgType = FAlgType
+  deriving (Show)
--- src/StaticCheck/StaticCheck.hs
+++ src/StaticCheck/StaticCheck.hs
@@ -5,4 +5,22 @@ import Bnfc.AbsHpl
 import Bnfc.ErrM
 
 staticCheck :: Program -> Err ProgramFormat
-staticCheck = undefined
+staticCheck p = staticCheckErrPack $ staticCheckEither p
+
+staticCheckErrPack :: Either String ProgramFormat -> Err ProgramFormat
+staticCheckErrPack (Left e) = Bad e
+staticCheckErrPack (Right r) = Ok r
+
+staticCheckEither :: Program -> Either String ProgramFormat
+staticCheckEither p = do
+  let pf = initialConvertToProgramFormat p
+  fail $ show pf
+  return pf
+
+initialConvertToProgramFormat :: Program -> ProgramFormat
+initialConvertToProgramFormat (ProgramB l) = SITList $ map absSITToSit l
+
+absSITToSit :: StructOrInterfaceOrType -> SIT
+absSITToSit (StructOrInterfaceOrTypeS s) = SITStruct FStruct
+
+setArithmetics = undefined
