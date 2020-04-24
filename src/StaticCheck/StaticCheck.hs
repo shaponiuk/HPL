4d655c8e6f22c1c7142785a23311cd8c6ef9554a
SzymonHaponiuk <shaponiuk@linux.home>
2020-04-24 14:22:03 +0200

    .

--- src/StaticCheck/StaticCheck.hs
+++ src/StaticCheck/StaticCheck.hs
@@ -22,15 +22,37 @@ initialConvertToProgramFormat (ProgramB l) = SITList $ map absSITToSit l
 
 absSITToSit :: StructOrInterfaceOrType -> SIT
 absSITToSit (StructOrInterfaceOrTypeS s) = SITStruct $ convertStruct s
+absSITToSit (StructOrInterfaceOrTypeI i) = undefined
+absSITToSit (StructOrInterfaceOrTypeT t) = undefined
 
 convertStruct :: Struct -> FStruct
-convertStruct (StructB (Ident name) body) = FStructB name $ convertBody body
+convertStruct (StructB (Ident name) body) = 
+  FStructB name $ convertBody body
+convertStruct (StructI (Ident name) interfaceIdList body) = 
+  FStructI name (convertInterfaceIdList interfaceIdList) $ convertBody body
+
+convertInterfaceIdList :: [InterfaceId] -> [String]
+convertInterfaceIdList list = convertIdentList $ map unwrapInterfaceId list
+
+unwrapInterfaceId :: InterfaceId -> Ident
+unwrapInterfaceId (InterfaceIdB ident) = ident
+
+convertIdentList :: [Ident] -> [String]
+convertIdentList [] = []
+convertIdentList ((Ident str):xs) = str:(convertIdentList xs)
 
 convertBody :: StructBody -> FStructBody
 convertBody (StructBodyB structFieldList) = 
   FStructBody $ map convertStructField structFieldList
 
 convertStructField :: StructField -> FStructField
-convertStructField = undefined
+convertStructField (StructFieldFunPr fd) = FStructFieldFunPrivate $ convertFunctionDef fd
+convertStructField (StructFieldFunPu fd) = FStructFieldFunPublic $ convertFunctionDef fd
+convertStructField (StructFieldRefPr ref) = FStructFieldRefPrivate $ convertRefDef ref
+convertStructField (StructFieldRefPu ref) = FStructFieldRefPublic $ convertRefDef ref
+
+convertFunctionDef = undefined
+
+convertRefDef = undefined
 
 setArithmetics = undefined
