bfc2ab7eb5bde1edf3e9724a6c6ff89c57612573
Szymon Haponiuk <haponiukszymon@gmail.com>
2020-05-04 23:54:57 +0200

    .

--- bnfc/SkelHpl.hs
+++ bnfc/SkelHpl.hs
@@ -11,163 +11,85 @@ failure x = Bad $ "Undefined case: " ++ show x
 
 transIdent :: Ident -> Result
 transIdent x = case x of
-  Ident str  -> failure x
-
-
+  Ident string -> failure x
 transProgram :: Program -> Result
 transProgram x = case x of
-  ProgramB structorinterfaceortypes  -> failure x
-
-
-transStructOrInterfaceOrType :: StructOrInterfaceOrType -> Result
-transStructOrInterfaceOrType x = case x of
-  StructOrInterfaceOrTypeS struct  -> failure x
-  StructOrInterfaceOrTypeI interface  -> failure x
-  StructOrInterfaceOrTypeT algtype  -> failure x
-
-
-transStruct :: Struct -> Result
-transStruct x = case x of
-  StructB id structbody  -> failure x
-  StructI id interfaceids structbody  -> failure x
-
-
-transStructBody :: StructBody -> Result
-transStructBody x = case x of
-  StructBodyB structfields  -> failure x
-
-
-transStructField :: StructField -> Result
-transStructField x = case x of
-  StructFieldFunPr functiondef  -> failure x
-  StructFieldFunPu functiondef  -> failure x
-  StructFieldRefPr refdef  -> failure x
-  StructFieldRefPu refdef  -> failure x
-
-
-transInterfaceId :: InterfaceId -> Result
-transInterfaceId x = case x of
-  InterfaceIdB id  -> failure x
-
-
-transInterface :: Interface -> Result
-transInterface x = case x of
-  InterfaceB interfaceid interfacebody  -> failure x
-  InterfaceBInh interfaceid interfaceids interfacebody  -> failure x
-
-
-transInterfaceBody :: InterfaceBody -> Result
-transInterfaceBody x = case x of
-  InterfaceBodyB funorrefdecls  -> failure x
-
-
-transFunOrRefDecl :: FunOrRefDecl -> Result
-transFunOrRefDecl x = case x of
-  FunOrRefDeclF type' id  -> failure x
-  FunOrRefDeclSF type' id  -> failure x
-  FunOrRefDeclR type' id  -> failure x
-
-
+  ProgramB functionorrefortypes -> failure x
+transFunctionOrRefOrType :: FunctionOrRefOrType -> Result
+transFunctionOrRefOrType x = case x of
+  FunctionOrRefOrTypeF functiondef -> failure x
+  FunctionOrRefOrTypeT algtype -> failure x
+  FunctionOrRefOrTypeR refdef -> failure x
 transFunctionDef :: FunctionDef -> Result
 transFunctionDef x = case x of
-  FunctionDefB type' id functionargs valuestatement  -> failure x
-  SusFunctionDef functiondef  -> failure x
-
-
+  FunctionDefB type_ ident functionargs valuestatement -> failure x
+  SusFunctionDef type_ ident functionargs valuestatement -> failure x
 transFunctionArg :: FunctionArg -> Result
 transFunctionArg x = case x of
-  FunctionArgB patternmatch  -> failure x
-
-
+  FunctionArgB patternmatch -> failure x
 transValueStatement :: ValueStatement -> Result
 transValueStatement x = case x of
-  ValueStatementB assignments valuestatement  -> failure x
-  ForceValueStatement assignments valuestatement  -> failure x
-  IfValueStatement valuestatement0 valuestatement1 valuestatement  -> failure x
-  LValueStatement listvaluestatementr  -> failure x
-  TValueStatement tuplevaluestatementr  -> failure x
-  AValueStatement funapplication  -> failure x
-  IValueStatement n  -> failure x
-  LitStrValueStatement str  -> failure x
-  FValueStatement id valuestatement  -> failure x
-  Expr valuestatement valuestatementexpr  -> failure x
-
-
+  ValueStatementB assignments valuestatement -> failure x
+  ForceValueStatement assignments valuestatement -> failure x
+  IfValueStatement valuestatement1 valuestatement2 valuestatement3 -> failure x
+  LValueStatement listvaluestatementr -> failure x
+  TValueStatement tuplevaluestatementr -> failure x
+  AValueStatement funapplication -> failure x
+  IValueStatement integer -> failure x
+  LitStrValueStatement string -> failure x
+  FValueStatement ident valuestatement -> failure x
+  Expr valuestatement valuestatementexpr -> failure x
 transRefDef :: RefDef -> Result
 transRefDef x = case x of
-  RefDefB type' id valuestatement  -> failure x
-
-
+  RefDefB type_ ident valuestatement -> failure x
 transType :: Type -> Result
 transType x = case x of
-  TypeB id types  -> failure x
-  FunType type'0 type'  -> failure x
-  TType types  -> failure x
-
-
+  TypeB ident types -> failure x
+  FunType type_1 type_2 -> failure x
+  TType types -> failure x
 transAlgType :: AlgType -> Result
 transAlgType x = case x of
-  AlgTypeB id typeargs algtypevals  -> failure x
-
-
+  AlgTypeB ident typeargs algtypevals -> failure x
 transTypeArg :: TypeArg -> Result
 transTypeArg x = case x of
-  TypeArgB id  -> failure x
-
-
+  TypeArgB ident -> failure x
 transAlgTypeVal :: AlgTypeVal -> Result
 transAlgTypeVal x = case x of
-  AlgTypeValB id type'  -> failure x
-
-
+  AlgTypeValB ident type_ -> failure x
 transPatternMatch :: PatternMatch -> Result
 transPatternMatch x = case x of
-  PatternMatchI n  -> failure x
-  PatternMatchB id  -> failure x
-  TPatternMatch patternmatchs  -> failure x
-  CPatternMatch patternmatch patternmatchs  -> failure x
-
-
+  PatternMatchI integer -> failure x
+  PatternMatchB ident -> failure x
+  TPatternMatch patternmatchs -> failure x
+  CPatternMatch patternmatch patternmatchs -> failure x
 transAssignment :: Assignment -> Result
 transAssignment x = case x of
-  AssignmentB type' patternmatch valuestatement  -> failure x
-  RefAssignment refdef  -> failure x
-
-
+  AssignmentB type_ patternmatch valuestatement -> failure x
+  RefAssignment refdef -> failure x
 transFunApplication :: FunApplication -> Result
 transFunApplication x = case x of
-  SFunApplication id funapplication  -> failure x
-  FunApplicationB id functionargappls  -> failure x
-
-
+  SFunApplication ident funapplication -> failure x
+  FunApplicationB ident functionargappls -> failure x
 transFunctionArgAppl :: FunctionArgAppl -> Result
 transFunctionArgAppl x = case x of
-  FunctionArgApplB valuestatement  -> failure x
-
-
+  FunctionArgApplB valuestatement -> failure x
 transListValueStatementr :: ListValueStatementr -> Result
 transListValueStatementr x = case x of
-  ListValueStatementB valuestatements  -> failure x
-
-
+  ListValueStatementB valuestatements -> failure x
 transTupleValueStatementr :: TupleValueStatementr -> Result
 transTupleValueStatementr x = case x of
-  TupleValueStatementB valuestatements  -> failure x
-
-
+  TupleValueStatementB valuestatements -> failure x
 transValueStatementExpr :: ValueStatementExpr -> Result
 transValueStatementExpr x = case x of
-  EAdd valuestatement  -> failure x
-  ESub valuestatement  -> failure x
-  EMod valuestatement  -> failure x
-  EMul valuestatement  -> failure x
-  EDiv valuestatement  -> failure x
-  EL valuestatement  -> failure x
-  ELQ valuestatement  -> failure x
-  EG valuestatement  -> failure x
-  EGQ valuestatement  -> failure x
-  EEQ valuestatement  -> failure x
-  ENE valuestatement  -> failure x
-
-
+  EAdd valuestatement -> failure x
+  ESub valuestatement -> failure x
+  EMod valuestatement -> failure x
+  EMul valuestatement -> failure x
+  EDiv valuestatement -> failure x
+  EL valuestatement -> failure x
+  ELQ valuestatement -> failure x
+  EG valuestatement -> failure x
+  EGQ valuestatement -> failure x
+  EEQ valuestatement -> failure x
+  ENE valuestatement -> failure x
 
